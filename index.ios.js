e409e970b0935ebe246d6bb6a333ab5979c53880
Vadim Sikora <vadim.sikora@divio.ch>
2015-11-22 16:53:49 +0100

    some animations + es6

--- index.ios.js
+++ index.ios.js
@@ -2,48 +2,83 @@ import React, {
     AppRegistry,
     Text,
     View,
-    TextInput
+    TextInput,
+    Dimensions,
+    DeviceEventEmitter,
+    Animated,
+    Easing
 } from 'react-native';
 import Button from 'apsl-react-native-button';
 import styles from './styles/root';
 // import { Provider } from 'react-redux/native';
 
-const DivioGithubCalculator = React.createClass({
-    getInitialState() {
-        return {
-            text: ''
+class DivioGithubCalculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+            visibleHeight: new Animated.Value(Dimensions.get('window').height)
         };
-    },
+    }
 
     _handlePress() {
-        this.setState({ isLoading: true });
-    },
+        if (this.state.text) {
+            this.setState({ isLoading: true });
+        }
+    }
+
+    componentWillMount() {
+        DeviceEventEmitter.addListener('keyboardWillShow', this.keyboardWillShow.bind(this));
+        DeviceEventEmitter.addListener('keyboardWillHide', this.keyboardWillHide.bind(this));
+    }
+
+    keyboardWillShow(e) {
+        const newSize = Dimensions.get('window').height - e.endCoordinates.height;
+
+        Animated.timing(this.state.visibleHeight, {
+            duration: 250,
+            easing: Easing.inOut(Easing.ease),
+            toValue: newSize
+        }).start();
+    }
+
+    keyboardWillHide() {
+        const newSize = Dimensions.get('window').height;
+
+        Animated.timing(this.state.visibleHeight, {
+            duration: 250,
+            easing: Easing.inOut(Easing.ease),
+            toValue: newSize
+        }).start();
+    }
 
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Divio Github Calculator
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, enter your Github username.
-                </Text>
-                <TextInput
-                    style={[styles.input, this.state.isLoading ? styles.inputDisabled : {}]}
-                    onChangeText={text => this.setState({ text })}
-                    onBlur={() => this._handlePress()}
-                    editable={!this.state.isLoading}
-                    value={this.state.text} />
-                <Button
-                    style={styles.button}
-                    textStyle={styles.buttonText}
-                    isLoading={this.state.isLoading}
-                    onPress={() => this._handlePress()}>
-                    Check my score!
-                </Button>
+            <View>
+                <Animated.View style={[styles.container, { height: this.state.visibleHeight }]}>
+                    <Text style={styles.welcome}>
+                        Divio Github Calculator
+                    </Text>
+                    <Text style={styles.instructions}>
+                        To get started, enter your Github username.
+                    </Text>
+                    <TextInput
+                        style={[styles.input, this.state.isLoading ? styles.inputDisabled : {}]}
+                        onChangeText={text => this.setState({ text })}
+                        onBlur={() => this._handlePress()}
+                        editable={!this.state.isLoading}
+                        value={this.state.text} />
+                    <Button
+                        style={styles.button}
+                        textStyle={styles.buttonText}
+                        isLoading={this.state.isLoading}
+                        onPress={() => this._handlePress()}>
+                        Check my score!
+                    </Button>
+                </Animated.View>
             </View>
         );
     }
-});
+}
 
 AppRegistry.registerComponent('DivioGithubCalculator', () => DivioGithubCalculator);
